96120801ddfefe5e183795d90b8a95c9b651ff98
Hong-sk <sooyun9600@naver.com>
2022-08-10 23:33:40 +0900

    fontawesome 적용, 배경색 random으 바꿈

--- client/src/components/TodoListItem.js
+++ client/src/components/TodoListItem.js
@@ -1,5 +1,9 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./TodoListItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 
 const TodoListItem = ({
   todoListItem,
@@ -7,10 +11,38 @@ const TodoListItem = ({
   updateTodoListItem,
 }) => {
   const { title, contents } = todoListItem;
+  const [todoChecked, setTodoChecked] = useState(false);
+
+  const handleClick = () => {
+    setTodoChecked(!todoChecked);
+  };
+
+  let colorNum = "color" + Math.floor(Math.random() * 10);
+
+  // const aaa = document.querySelector(".todo--list--item");
+  // if (setTodoChecked === false) {
+  //   aaa.style.filter = "grayscale(100%)";
+  // }
+
   return (
-    <div className="todo--list--item">
+    <div className="todo--list--item" id={colorNum}>
       <p>{title}</p>
       <p>{contents}</p>
+      <FontAwesomeIcon
+        className="pencil icon"
+        icon={faPencil}
+        onClick={updateTodoListItem}
+      />
+      <FontAwesomeIcon
+        className="trash icon"
+        icon={faTrashCan}
+        onClick={deleteTodoListItem}
+      />
+      <FontAwesomeIcon
+        className="checkbox icon"
+        icon={faSquareCheck}
+        onClick={handleClick}
+      />
     </div>
   );
 };
